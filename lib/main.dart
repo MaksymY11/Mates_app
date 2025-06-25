a56e6bd67eb771607878c635a977e16d54c90597
Maksym Yelisyeyev <myelisyeyev9498@sdsu.edu>
2025-06-25 12:36:54 -0700

    Added landing page. Changed main to set homepage depending on web or android/ios.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'signup_page.dart';
+import 'landing_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +10,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mates',
-      home: LoginPage(),
+      home: kIsWeb ? const LandingPage() : LoginPage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+      },
     );
   }
 }
